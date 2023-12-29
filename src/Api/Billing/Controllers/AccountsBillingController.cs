62889480b7d40016733b0fb490402c8d2a9bead3
Conner Turnbull <cturnbull@bitwarden.com>
2023-12-29 10:12:41 -0500

    Moved AccountsBillingController.cs

--- src/Api/Billing/Controllers/AccountsBillingController.cs
+++ src/Api/Billing/Controllers/AccountsBillingController.cs
@@ -4,7 +4,7 @@ using Bit.Core.Utilities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Bit.Api.Controllers;
+namespace Bit.Api.Billing.Controllers;
 
 [Route("accounts/billing")]
 [Authorize("Application")]
